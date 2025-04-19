564cfe038a56430d6e28725155575f5f784d9e93
Flo S. <157888733+Flow2510@users.noreply.github.com>
2025-04-19 17:33:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const markRead = document.querySelector('.mark_as_read');
 const newNotifs = document.querySelectorAll('.new_notif');
 const countResult = document.querySelector('.new_notifs_counter');
-const notifCount = document.querySelectorAll('.new_notif').length;
+
+const notifs = document.querySelectorAll('.notification');
 
 function updateCount(){
+    const notifCount = document.querySelectorAll('.new_notif').length;
     countResult.textContent = notifCount ;
+    countResult.style.display = notifCount === 0 ? "none" : "inline-block"; //si le compteur est a 0 le display passe a none ou
 }
 
 updateCount();
@@ -13,6 +16,15 @@ markRead.addEventListener('click', () =>{  //tout marquer comme lu
     newNotifs.forEach(notif => {
         notif.classList.remove('new_notif');
         countResult.style.display = "none";
+    })  
+    updateCount();
+})
+
+newNotifs.forEach(notif => {
+    notif.addEventListener('click', () => {
+        notif.classList.remove('new_notif');
+        updateCount();
     })
     
 })
+
